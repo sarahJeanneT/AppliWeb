b70c69a3f10a21f3a365627b9a47184914c97897
Sarah-Jeanne Tourangeau <sarah-jeanne.tourangeau@grenoble-inp.org>
2017-04-25 13:14:50 +0200

    mustache

--- jodle/platforms/android/assets/www/js/index.js
+++ jodle/platforms/android/assets/www/js/index.js
@@ -2,11 +2,21 @@ document.addEventListener('deviceready', onDeviceReady ,false)
 
 function onDeviceReady ()
 {
-  alert('Volume Up Button is pressed !');
 
   $('#temporaire').click(function(){
-    alert('Volume Up Button is pressed !')
+    alert('temporaire')
     window.location = "page2.html"
   })
+
+  $('#connect_form').submit(function(e){
+    alert('connecter')
+    window.location = "page1.html"
+    $.get("jodle/messages", "", connect, "html");
+    e.preventDefault();
+  })
+
+}
+
+function connect(){
 
 }
--- jodle/www/js/index.js
+++ jodle/www/js/index.js
@@ -2,11 +2,20 @@ document.addEventListener('deviceready', onDeviceReady ,false)
 
 function onDeviceReady ()
 {
-  alert('Volume Up Button is pressed !');
 
   $('#temporaire').click(function(){
-    alert('Volume Up Button is pressed !')
+    alert('temporaire')
     window.location = "page2.html"
   })
 
+  $('#connect_form').submit(function(e){
+
+    $.get("jodle/messages", "", connect, "html");
+    e.preventDefault();
+  })
+
+}
+
+function connect(){
+ window.location = "page1.html"
 }
